d8a1e7dd5f36118d43a8c20cb3f9a903185598bc
grechaa82 <danilgusejnov@gmail.com>
2023-02-20 22:32:52 +0300

    "Format words into a sentence" and "Mexican wave"

---
+++ Codewars/Format_words_into_a_sentence/Kata.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace Format_words_into_a_sentence
+{
+    public class Kata
+    {
+        public static string FormatWords(string[] words)
+        {
+            if (words is null)
+            {
+                return "";
+            }
+
+            var temporaryWords = new List<string>();
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(words[i]))
+                {
+                    temporaryWords.Add(words[i]);
+                }
+            }
+
+            for (int i = 0; i < temporaryWords.Count; i++)
+            {
+                if (result.Length == 0)
+                {
+                    result.Append(temporaryWords[i]);
+                }
+                else
+                {
+                    if (i != temporaryWords.Count - 1)
+                    {
+                        result.Append(", ");
+                    }
+                    else
+                    {
+                        result.Append(" and ");
+                    }
+
+                    result.Append(temporaryWords[i]);
+                }
+
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Codewars/Mexican_wave/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Mexican_wave
+{
+    public class Kata
+    {
+        public List<string> wave(string str)
+        {
+            var result = new List<string>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    var symbol = Char.ToUpper(str[i]);
+                    StringBuilder newStr = new StringBuilder(str);
+                    newStr[i] = symbol;
+                    result.Add(newStr.ToString());
+                }
+            }
+
+            return result;
+        }
+    }
+}
