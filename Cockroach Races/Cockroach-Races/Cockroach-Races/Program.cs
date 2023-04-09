caa06b9dd37ac505214feae0770c236966a15c08
Grechaa8.2 <danilgusejnov@gmail.com>
2023-04-09 15:46:45 +0300

    Cockroach Races v1

---
+++ Cockroach Races/Cockroach-Races/Cockroach-Races/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    private static int numberOfThreads = 5;
+
+    static void Main()
+    {
+        var threads = CreateThead(numberOfThreads);
+
+        for (int i = 0; i < threads.Count; i++)
+        {
+            threads[i].Start();
+        }
+
+        while (true) ;
+    }
+
+    private static List<Thread> CreateThead(int numberOfThreads)
+    {
+        var threads = new List<Thread>();
+
+        for (int i = 0; i < numberOfThreads; i++)
+        {
+            var t = new Thread(printThread);
+            threads.Add(t);
+        }
+
+        return threads;
+    }
+
+    private static void printThread()
+    {
+        var positionLeft = 0;
+        var positionTop = Thread.CurrentThread.ManagedThreadId;
+
+        for (int i = 0; i < 50; i++)
+        {
+            Console.SetCursorPosition(positionLeft + 1, positionTop);
+            Console.Write("@");
+            Console.SetCursorPosition(positionLeft, positionTop);
+            Console.Write(" ");
+            Thread.Sleep(100);
+            ++positionLeft;
+        }
+    }
+}
